5ae17298a3aadd44197d8db05d6a4b93168e0198
ethiraj <esrinivasan_c@rpxcorp.com>
2015-11-26 17:37:53 +0530

    error fix

--- app/controllers/collection2csv_controller.rb
+++ app/controllers/collection2csv_controller.rb
@@ -4,21 +4,22 @@ class Collection2csvController < ApplicationController
     klass =  params[:klass].constantize
     if params[:column_names].present?
       column_names = params[:column_names].split(',').to_a
-   	else 
+   	else
    		column_names = klass.column_names
    	end
    	collection = klass.find(params[:ids])
-	  respond_to do |format|
-	    format.csv { 
-	    	send_data (CSV.generate do |csv|
-	    		csv << column_names.map(&:humanize)
-	    		collection.each do |product|
-	      			csv << product.attributes.values_at(*column_names)
-	    		end
-	  		end 
-	  		) ,:filename => "#{klass}_#{DateTime.now.to_formatted_s(:db)}"
+   	respond_to do |format|
+	    format.csv { send_data to_csv(collection,column_names)  ,:filename => "#{klass}_#{DateTime.now.to_formatted_s(:db)}.csv"
+			}
+		end
+	end
 
-	  	}
+	def to_csv(collection,column_names)
+ 	  CSV.generate do |csv|
+	  	  csv << column_names.map(&:humanize)
+	    collection.each do |product|
+	      csv << product.attributes.values_at(*column_names)
+	    end
 		end
 	end
 end
