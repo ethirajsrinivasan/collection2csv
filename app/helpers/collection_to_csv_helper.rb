9fb4a1f19a63e9ab6312abbd0d82cd5af1f8b519
ethiraj <ethirajsrinivasan@gmail.com>
2016-06-22 17:52:54 +0530

    associations added (#1)

--- app/helpers/collection_to_csv_helper.rb
+++ app/helpers/collection_to_csv_helper.rb
@@ -1,8 +1,11 @@
 module CollectionToCsvHelper
-  def collection_download(collection,column_names={})
+  def collection_download(collection,options={})
+    column_names = options[:columns].presence
+    associations = options[:associations].presence
+    link_text = options[:link_text].presence || 'Download'
     klass =  collection.try(:first).class
     raise "#{collection} is not a ActiveRecord collection" unless klass.ancestors.include?(ActiveRecord::Base)
     collection_ids = collection.map(&:id)
-    link_to "Download" , collection2csv_path(ids: collection_ids,klass: klass,format: "csv",column_names: column_names)
+    link_to link_text , collection2csv_path(ids: collection_ids,klass: klass,format: "csv",column_names: column_names,associations: associations)
   end
 end
--- lib/collection2csv/version.rb
+++ lib/collection2csv/version.rb
@@ -1,3 +1,3 @@
 module Collection2csv
-  VERSION = "0.1.4"
+  VERSION = "0.1.5"
 end
